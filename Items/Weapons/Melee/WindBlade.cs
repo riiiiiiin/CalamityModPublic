48e30df168d6ef7e3a5efa15408e979349303748
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2024-05-22 15:42:52 -0400

    Removed Wind Blade rework from main branch
    Hi to people who snoop around in the source I guess

--- Items/Weapons/Melee/WindBlade.cs
+++ Items/Weapons/Melee/WindBlade.cs
@@ -1,6 +1,4 @@
-﻿using CalamityMod.Items.BaseItems;
-using CalamityMod.Items.Materials;
-using CalamityMod.Projectiles.BaseProjectiles;
+﻿using CalamityMod.Items.Materials;
 using CalamityMod.Projectiles.Melee;
 using Microsoft.Xna.Framework;
 using Terraria;
@@ -10,29 +8,38 @@ using Terraria.ModLoader;
 
 namespace CalamityMod.Items.Weapons.Melee
 {
-    public class WindBlade : CustomUseProjItem, ILocalizedModType
+    public class WindBlade : ModItem, ILocalizedModType
     {
         public new string LocalizationCategory => "Items.Weapons.Melee";
         public override void SetDefaults()
         {
             Item.width = 58;
             Item.height = 58;
-            Item.damage = 42;
+            Item.damage = 41;
             Item.DamageType = DamageClass.Melee;
-            Item.useAnimation = 18;
+            Item.useAnimation = 20;
             Item.useStyle = ItemUseStyleID.Swing;
-            Item.useTime = 18;
+            Item.useTime = 20;
             Item.useTurn = true;
             Item.knockBack = 5f;
+            Item.UseSound = SoundID.Item1;
             Item.autoReuse = true;
             Item.value = CalamityGlobalItem.RarityOrangeBuyPrice;
             Item.rare = ItemRarityID.Orange;
+            Item.shoot = ModContent.ProjectileType<Cyclone>();
+            Item.shootSpeed = 3f;
+        }
 
-            Item.channel = true;
-            Item.shoot = ModContent.ProjectileType<WindBladeHoldout>();
-            Item.noUseGraphic = true;
-            Item.noMelee = true;
-            Item.useStyle = ItemUseStyleID.Shoot;
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            Projectile.NewProjectile(source, position, velocity, type, damage / 2, knockback, player.whoAmI);
+            return false;
+        }
+
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            if (Main.rand.NextBool(3))
+                Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, DustID.BlueTorch);
         }
 
         public override void AddRecipes()
---
+++ Projectiles/Melee/Cyclone.cs
@@ -0,0 +1,125 @@
+﻿using CalamityMod.CalPlayer;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Audio;
+
+namespace CalamityMod.Projectiles.Melee
+{
+    public class Cyclone : ModProjectile, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Projectiles.Melee";
+        public int dustvortex = 0;
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 6;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 56;
+            Projectile.height = 56;
+            Projectile.alpha = 255;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.timeLeft = 300;
+            Projectile.extraUpdates = 2;
+            Projectile.penetrate = 2;
+            Projectile.ignoreWater = true;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+            Projectile.tileCollide = false;
+        }
+
+        public override void AI()
+        {
+            Projectile.ai[0]++;
+            Projectile.ai[1]++;
+
+            //Code so it doesnt collide on tiles instantly
+            if (Projectile.ai[0] >= 12)
+                Projectile.tileCollide = true;
+
+            Projectile.rotation += 2.5f;
+            Projectile.alpha -= 5;
+            if (Projectile.alpha < 50)
+            {
+                Projectile.alpha = 50;
+                if (Projectile.ai[1] >= 15)
+                {
+
+                    for (int i = 1; i <= 6; i++)
+                    {
+                        Vector2 dustspeed = new Vector2(3f, 3f).RotatedBy(MathHelper.ToRadians(dustvortex));
+                        int d = Dust.NewDust(Projectile.Center, Projectile.width / 2, Projectile.height / 2, 31, dustspeed.X, dustspeed.Y, 200, new Color(232, 251, 250, 200), 1.3f);
+                        Main.dust[d].noGravity = true;
+                        Main.dust[d].velocity = dustspeed;
+                        dustvortex += 60;
+                    }
+                    dustvortex -= 355;
+                    Projectile.ai[1] = 0;
+                }
+            }
+            float num472 = Projectile.Center.X;
+            float num473 = Projectile.Center.Y;
+            float num474 = 600f;
+            for (int num475 = 0; num475 < Main.maxNPCs; num475++)
+            {
+                NPC npc = Main.npc[num475];
+                if (npc.CanBeChasedBy(Projectile, false) && Collision.CanHit(Projectile.Center, 1, 1, npc.Center, 1, 1) && !CalamityPlayer.areThereAnyDamnBosses)
+                {
+                    float npcCenterX = npc.position.X + (float)(npc.width / 2);
+                    float npcCenterY = npc.position.Y + (float)(npc.height / 2);
+                    float num478 = Math.Abs(Projectile.position.X + (float)(Projectile.width / 2) - npcCenterX) + Math.Abs(Projectile.position.Y + (float)(Projectile.height / 2) - npcCenterY);
+                    if (num478 < num474)
+                    {
+                        if (npc.position.X < num472)
+                        {
+                            npc.velocity.X += 0.05f;
+                        }
+                        else
+                        {
+                            npc.velocity.X -= 0.05f;
+                        }
+                        if (npc.position.Y < num473)
+                        {
+                            npc.velocity.Y += 0.05f;
+                        }
+                        else
+                        {
+                            npc.velocity.Y -= 0.05f;
+                        }
+                    }
+                }
+            }
+        }
+
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return new Color(204, 255, 255, Projectile.alpha);
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            CalamityUtils.DrawAfterimagesCentered(Projectile, ProjectileID.Sets.TrailingMode[Projectile.type], lightColor, 1);
+            return false;
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            SoundEngine.PlaySound(SoundID.Item60 with { Volume = SoundID.Item60.Volume * 0.6f }, Projectile.Center);
+
+            for (int i = 0; i <= 360; i += 3)
+            {
+                Vector2 dustspeed = new Vector2(3f, 3f).RotatedBy(MathHelper.ToRadians(i));
+                int d = Dust.NewDust(Projectile.Center, Projectile.width, Projectile.height, 31, dustspeed.X, dustspeed.Y, 200, new Color(232, 251, 250, 200), 1.4f);
+                Main.dust[d].noGravity = true;
+                Main.dust[d].position = Projectile.Center;
+                Main.dust[d].velocity = dustspeed;
+            }
+        }
+    }
+}
